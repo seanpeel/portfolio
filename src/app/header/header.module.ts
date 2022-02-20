fdc93ba513dfee4d03b09883db99a42317c12277
Sean Peel <smp0924@p2es.com>
2022-02-19 19:02:55 -0700

    remove top-menu-component and header css changes

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -6,12 +6,10 @@ import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HeaderComponent } from './header.component';
-import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    TopMenuComponent
+    HeaderComponent
   ],
   exports: [
     HeaderComponent
